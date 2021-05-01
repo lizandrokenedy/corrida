e5430a16bec83207e833fde696bf87cdd79d2a96
Lizandro Kenedy <lizandrosantos@live.com>
2021-04-30 23:50:58 -0400

    Documentando api corredores

--- app/Http/Controllers/CorredorController.php
+++ app/Http/Controllers/CorredorController.php
@@ -19,9 +19,47 @@ class CorredorController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @OA\Get(
+     *   path="/api/corredor",
+     *   tags={"Corredor"},
+     *   summary="Lista de corredores",
+     *   @OA\Response(
+     *    response=200,
+     *    description="Sucesso",
+     *      @OA\JsonContent(
+     *         @OA\Property(property="data", type="object", example={
+     *                {
+     *                  "id": 1,
+     *                  "nome": "Lizandro",
+     *                  "cpf": "04397588157",
+     *                  "data_nascimento": "1994-07-11T03:00:00.000000Z",
+     *                  "idade": 18,
+     *                  "created_at": "2021-04-28T21:14:54.000000Z",
+     *                  "updated_at": "2021-04-28T21:14:54.000000Z"
+     *                },
+     *                {
+     *                  "id": 2,
+     *                  "nome": "Lizandro",
+     *                  "cpf": "04397588156",
+     *                  "data_nascimento": "1994-07-11T03:00:00.000000Z",
+     *                  "idade": 18,
+     *                  "created_at": "2021-04-28T21:15:00.000000Z",
+     *                  "updated_at": "2021-04-28T21:15:00.000000Z"
+     *                },
+     *      }),
+     *         @OA\Property(property="mensagem", type="string", example="Operação realizada com sucesso."),
+     *         @OA\Property(property="sucesso", type="bool", example="true")
+     *      )
+     *   ),
+     *   @OA\Response(
+     *    response=400,
+     *    description="Erro",
+     *      @OA\JsonContent(
+     *         @OA\Property(property="mensagem", type="string", example="Erro ao realizar operação."),
+     *         @OA\Property(property="sucesso", type="bool", example="false")
+     *      )
+     *   ),
+     * )
      */
     public function index()
     {
@@ -33,10 +71,38 @@ class CorredorController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @OA\Post(
+     *   path="/api/corredor",
+     *   tags={"Corredor"},
+     *   summary="Cria corredor",
+     *   @OA\RequestBody(
+     *    required=true,
+     *    description="Exemplo para gerar classificações",
+     *    @OA\JsonContent(
+     *       required={"nome", "cpf", "data_nascimento"},
+     *       @OA\Property(property="nome", type="string", format="string", example="Nome do candidato"),
+     *       @OA\Property(property="cpf", type="string", format="string", example="00000000000"),
+     *       @OA\Property(property="data_nascimento", type="string", format="string", example="1994-11-07")
+     *    ),
+     *  ),
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     *   @OA\Response(
+     *    response=200,
+     *    description="Sucesso",
+     *      @OA\JsonContent(
+     *         @OA\Property(property="mensagem", type="string", example="Operação realizada com sucesso."),
+     *         @OA\Property(property="sucesso", type="bool", example="true")
+     *      )
+     *   ),
+     *   @OA\Response(
+     *    response=400,
+     *    description="Erro",
+     *      @OA\JsonContent(
+     *         @OA\Property(property="mensagem", type="string", example="Erro ao realizar operação."),
+     *         @OA\Property(property="sucesso", type="bool", example="false")
+     *      )
+     *   ),
+     * )
      */
     public function store(Request $request)
     {
